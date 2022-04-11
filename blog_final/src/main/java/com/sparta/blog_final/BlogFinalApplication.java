08790720fca1fa5763658e2f0b9b529904a9def3
ggongdori <soominlee0410@gmail.com>
2022-04-11 11:51:20 +0900

    작성시간 modifiedAt, timestamped 쓸 때@enablejpaauditing 까먹지 말기!!!!!!!!!!!

--- blog_final/src/main/java/com/sparta/blog_final/BlogFinalApplication.java
+++ blog_final/src/main/java/com/sparta/blog_final/BlogFinalApplication.java
@@ -2,7 +2,9 @@ package com.sparta.blog_final;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BlogFinalApplication {
 
