0bf2c2782be18ec335a54c4fa1f9de529e7950d6
ggongdori <soominlee0410@gmail.com>
2022-04-11 10:49:18 +0900

    blog_final initial commit

---
+++ blog_final/src/main/java/com/sparta/blog_final/controller/PostController.java
@@ -0,0 +1,49 @@
+package com.sparta.blog_final.controller;
+
+
+import com.sparta.myblogtest1.models.Post;
+import com.sparta.myblogtest1.models.PostRepository;
+import com.sparta.myblogtest1.models.PostRequestDto;
+import com.sparta.myblogtest1.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostRepository postRepository;
+    private final PostService postService;
+
+    @GetMapping("/api/posts")
+    public List<Post> getPosts() {
+        return postRepository.findAllByOrderByCreatedAtDesc();
+    }
+
+    @PostMapping("/api/posts")
+    public Post createPost(@RequestBody PostRequestDto postRequestDto) {
+        Post post = new Post(postRequestDto);
+        postRepository.save(post);
+        return post;
+    }
+
+    @DeleteMapping("api/posts/{id}")
+    public Long deletePost(@PathVariable Long id) {
+        postRepository.deleteById(id);
+        return id;
+    }
+
+    @PutMapping("api/posts/{id}")
+    public Long updatePost(@PathVariable Long id, @RequestBody PostRequestDto postRequestDto) {
+        return postService.update(id, postRequestDto);
+    }
+
+    @GetMapping("api/posts/{id}")
+    public Post readPost(@PathVariable Long id) {
+        return postRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다")
+        );
+    }
+}
