93b763462251d9905137fad21629420e8c63ab56
ggongdori <soominlee0410@gmail.com>
2022-04-11 11:28:02 +0900

    blog_final 타임리프 디렉토리 추가

---
+++ blog_final/src/main/java/com/sparta/blog_final/BlogFinalApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.blog_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogFinalApplication.class, args);
+    }
+
+}
---
+++ blog_final/src/test/java/com/sparta/blog_final/BlogFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.blog_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
