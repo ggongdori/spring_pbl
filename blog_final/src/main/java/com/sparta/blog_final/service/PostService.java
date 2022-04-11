0dd55faeea7b50e114c8a5a04adb811b62275774
ggongdori <soominlee0410@gmail.com>
2022-04-11 11:00:48 +0900

    blog_final initial commit

---
+++ blog_final/src/main/java/com/sparta/blog_final/service/PostService.java
@@ -0,0 +1,25 @@
+package com.sparta.blog_final.service;
+
+import com.sparta.myblogtest1.models.Post;
+import com.sparta.myblogtest1.models.PostRepository;
+import com.sparta.myblogtest1.models.PostRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    public Long update(Long id, PostRequestDto postRequestDto) {
+        Post post = postRepository.findById(id).orElseThrow(
+                () -> new NullPointerException("해당 아이디가 존재하지 않습니다.")
+        );
+        post.update(postRequestDto);
+        return id;
+    }
+}
