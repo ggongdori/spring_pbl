eb06c2c5ca25fd363fda1f5c1cc61ffe3f81cfb5
ggongdori <soominlee0410@gmail.com>
2022-04-14 21:45:24 +0900

    spring security files added and modified

--- springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
+++ springcore/src/main/java/com/sparta/springcore/security/WebSecurityConfig.java
@@ -8,19 +8,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-// 어떤 요청이든 '인증'
+                // 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
                 .and()
-// 로그인 기능 허용
+                // 로그인 기능 허용
                 .formLogin()
                 .defaultSuccessUrl("/")
                 .permitAll()
                 .and()
-// 로그아웃 기능 허용
+                // 로그아웃 기능 허용
                 .logout()
                 .permitAll();
     }
