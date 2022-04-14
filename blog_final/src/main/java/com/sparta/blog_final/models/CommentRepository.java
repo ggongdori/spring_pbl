c40256ed16c807dccddd21f719e7df1b69f68db7
ggongdori <soominlee0410@gmail.com>
2022-04-14 15:28:02 +0900

    주석 추가 0414 15:27 version

--- blog_final/src/main/java/com/sparta/blog_final/models/CommentRepository.java
+++ blog_final/src/main/java/com/sparta/blog_final/models/CommentRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+    //CreatedAt -> ModifiedAt, 댓글 수정 후 새로 정렬, 근데 안 됨, 아마도 프론트 단에서 ModifiedAt 값을 받지 않아서
     List<Comment> findAllByOrderByModifiedAtDesc();
 }
