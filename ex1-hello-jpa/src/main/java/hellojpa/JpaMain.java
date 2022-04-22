5e0ad95101652274c1ea067c00b11c23a770f09d
ggongdori <soominlee0410@gmail.com>
2022-04-22 21:36:01 +0900

    hello-jpa main modified

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,7 @@ import javax.persistence.Persistence;
 
 public class JpaMain {
     public static void main(String[] args) {
+
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager entityManager = emf.createEntityManager();
         entityManager.close();
