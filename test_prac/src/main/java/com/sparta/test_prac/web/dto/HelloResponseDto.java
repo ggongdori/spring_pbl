9ba0201b7a7b39b749b1a50bb36dbf9ae3204fe1
ggongdori <soominlee0410@gmail.com>
2022-04-23 11:27:58 +0900

    HelloResponseDto added

---
+++ test_prac/src/main/java/com/sparta/test_prac/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.sparta.test_prac.web.dto;
+
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor //선언된 모든 final필드 포함 생성자 만듦, final 아닌건 포함 x
+public class HelloResponseDto {
+
+    private final String name;
+    private final String amount;
+
+}
--- test_prac/src/test/java/com/sparta/test_prac/HelloControllerTest.java
+++ test_prac/src/test/java/com/sparta/test_prac/HelloControllerTest.java
@@ -1,11 +1,13 @@
-package com.sparta.test_prac.web;
+package com.sparta.test_prac;
 
+import com.sparta.test_prac.web.HelloController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
