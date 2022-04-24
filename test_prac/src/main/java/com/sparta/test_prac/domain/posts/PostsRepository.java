236e5768f40862e92c480cbe4a1485d38b8cd0d9
ggongdori <soominlee0410@gmail.com>
2022-04-24 21:13:36 +0900

    PostsRepository modified

--- test_prac/src/main/java/com/sparta/test_prac/domain/posts/PostsRepository.java
+++ test_prac/src/main/java/com/sparta/test_prac/domain/posts/PostsRepository.java
@@ -2,10 +2,5 @@ package com.sparta.test_prac.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/*
-mybatis에서의 Dao, DB Layer 접근자
- */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
-
-
 }
