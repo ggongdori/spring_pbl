a1efe0fd8ccd9d6981b3f730df55818bde712b43
ggongdori <soominlee0410@gmail.com>
2022-04-28 01:18:08 +0900

    member test class added

--- jpashop/src/test/java/com/example/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/com/example/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +13,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Rollback(value = false)//롤백 안 하고 싶을 때
 public class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
 
     @Test
-    @Transactional
+    @Transactional //transactional이 테스트에 있으면 테스트 끝난 후 rollback
     public void testMember() throws Exception{
         //given
         Member member = new Member();
@@ -29,6 +31,7 @@ public class MemberRepositoryTest {
         //then
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
 
     }
 
