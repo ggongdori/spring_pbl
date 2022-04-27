da88e2455730566fc13765f6cca43f99248d3b88
ggongdori <soominlee0410@gmail.com>
2022-04-28 01:32:42 +0900

    member

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,16 +2,45 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.sql.SQLOutput;
 
+
+/**
+ * 영속성 컨텍스트 = 엔티티를 영구 저장하는 환경
+ * EntityManager.persist(entity); //db에 저장이 아닌 영속성 컨텍스트에 저장한다는 뜻
+ *
+ */
 public class JpaMain {
     public static void main(String[] args) {
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager entityManager = emf.createEntityManager();
-        entityManager.close();
-        emf.close();
+        EntityManager em = emf.createEntityManager();
 
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //비영속
+            Member member1 = new Member();
+            member1.setId(101L);
+            member1.setName("HelloJPA");
+            
+            //영속
+            System.out.println("====before===");
+            em.persist(member1);
+            System.out.println("=====after=====");
+            em.find(Member.class, 101L);
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+
+        } finally {
+            em.close();
+        }
+        emf.close();
 
     }
 }
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,15 +1,32 @@
 package hellojpa;
 
+
+
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-
 @Entity
 public class Member {
-
 
     @Id
     private Long id;
     private String name;
 //Getter, Setter …
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
