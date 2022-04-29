7e3a1fe92cb9fad05883dbb2ed85353bcc5ba75f
ggongdori <soominlee0410@gmail.com>
2022-04-29 23:46:56 +0900

    modified

--- jpashop/src/main/java/com/example/jpabook/jpashop/controller/MemberForm.java
+++ jpashop/src/main/java/com/example/jpabook/jpashop/controller/MemberForm.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.NotEmpty;
 public class MemberForm {
     @NotEmpty(message = "이름을 입력해주세요")
     private String name;
-
     private String city;
     private String street;
     private String zipcode;
