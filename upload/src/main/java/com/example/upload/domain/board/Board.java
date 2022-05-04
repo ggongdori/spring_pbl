71060cd89cf7a0a29a48230cf10c6d18b79d0863
ggongdori <soominlee0410@gmail.com>
2022-05-04 22:33:41 +0900

    board impl started

--- upload/src/main/java/com/example/upload/domain/board/Board.java
+++ upload/src/main/java/com/example/upload/domain/board/Board.java
@@ -1,8 +1,27 @@
 package com.example.upload.domain.board;
 
 import com.example.upload.domain.Timestamped;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Entity
 public class Board extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
+
+    private String contents;
+    private List<String> images = new ArrayList<>();
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
 
 
 }
