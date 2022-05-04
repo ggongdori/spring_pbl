d0dccb47a4a7ab907a0b8c2a15bcec8e774eb2de
ggongdori <soominlee0410@gmail.com>
2022-05-05 00:40:35 +0900

    board impl

--- upload/src/main/java/com/example/upload/domain/board/BoardRepository.java
+++ upload/src/main/java/com/example/upload/domain/board/BoardRepository.java
@@ -1,5 +1,6 @@
-package com.example.upload.domain;
+package com.example.upload.domain.board;
 
+import com.example.upload.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Item, Long> {
---
+++ upload/src/main/java/com/example/upload/domain/board/BoardService.java
@@ -0,0 +1,6 @@
+package com.example.upload.domain.board;
+
+import com.example.upload.domain.Timestamped;
+
+public class BoardService extends Timestamped {
+}
