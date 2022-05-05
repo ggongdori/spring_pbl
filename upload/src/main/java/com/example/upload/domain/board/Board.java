19623b730edc7c569e733fc93941c012b099c854
ggongdori <soominlee0410@gmail.com>
2022-05-05 14:02:20 +0900

    board impl

--- upload/src/main/java/com/example/upload/domain/board/Board.java
+++ upload/src/main/java/com/example/upload/domain/board/Board.java
@@ -1,8 +1,10 @@
 package com.example.upload.domain.board;
 
 import com.example.upload.domain.Timestamped;
+import com.example.upload.domain.comment.Comment;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -17,7 +19,20 @@ public class Board extends Timestamped {
     @Column(name = "board_id")
     private Long id;
 
+    private String title;
     private String contents;
+    private List<MultipartFile> images = new ArrayList<>();
+
+//    @OneToMany
+//    private boolean bookmark;
+//    @OneToMany
+//    private List<Comment> comments = new ArrayList<>();
+
+    private Board(String title, String contents, List<MultipartFile> images) {
+        this.title = title;
+        this.contents = contents;
+        this.images = images;
+    }
 //    private List<String> images = new ArrayList<>();
 //    @ManyToOne(fetch = FetchType.LAZY)
 //    @JoinColumn(name = "user_id")
