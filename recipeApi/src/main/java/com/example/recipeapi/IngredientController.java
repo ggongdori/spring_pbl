1ab4cdd682e24aae1ea51c9295703aace972db7f
ggongdori <soominlee0410@gmail.com>
2022-05-11 09:55:29 +0900

    requestmapping added

--- recipeApi/src/main/java/com/example/recipeapi/IngredientController.java
+++ recipeApi/src/main/java/com/example/recipeapi/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
+@RequestMapping("/api")
 @RestController
 public class IngredientController {
 
@@ -14,14 +15,14 @@ public class IngredientController {
 
 
     //냉장고 재료 가져오기
-    @GetMapping("/api/recipe")
+    @GetMapping("/recipe")
     public List<Ingredient> getIngredient() {
         return ingredientRepository.findAll();
     }
 
 
     //냉장고 재료 추가하기
-    @PostMapping("/api/recipe")
+    @PostMapping("/recipe")
     public Ingredient createIngredient(@RequestBody IngredientDto ingredientDto) {
         Ingredient ingredient = new Ingredient(ingredientDto);
 
@@ -29,20 +30,20 @@ public class IngredientController {
     }
 
     //재료 선택 시 매뉴 data추출
-    @GetMapping("/api/recipe/{ingredient}")
+    @GetMapping("/recipe/{ingredient}")
     public List<String> onlymenu(@PathVariable String ingredient) {
         return ingredientService.findmenu(ingredient);
     }
 
 
     //메뉴 선택 시 레시피 추출
-    @GetMapping("/api/recipe/menu/{recipe}")
+    @GetMapping("/recipe/menu/{recipe}")
     public List<String> rcpmanual(@PathVariable String recipe){
         return ingredientService.menurecipe(recipe);
     }
 
     //냉장고 재료 삭제하기
-    @DeleteMapping("/api/recipe")
+    @DeleteMapping("/recipe")
     public String deleteIngredient(@RequestBody IngredientDto ingredientDto) {
         ingredientService.delete(ingredientDto);
 
