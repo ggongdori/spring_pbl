640eb133bbe6f2e512196e645b56a8d371e399c3
ggongdori <soominlee0410@gmail.com>
2022-05-12 20:58:18 +0900

    initial

--- spring-oauth/src/main/java/com/example/springoauth/TestApi.java
+++ spring-oauth/src/main/java/com/example/springoauth/TestApi.java
@@ -1,4 +1,10 @@
 package com.example.springoauth;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TestApi {
+    @GetMapping("/test")
+    public String index() {
+        return "Hello World";
+    }
 }
