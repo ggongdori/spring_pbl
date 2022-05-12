fadd092812ac6b5c8453e8a8431050a2a64ecaab
ggongdori <soominlee0410@gmail.com>
2022-05-12 20:58:28 +0900

    initial

--- spring-oauth/src/main/java/com/example/springoauth/TestApi.java
+++ spring-oauth/src/main/java/com/example/springoauth/TestApi.java
@@ -1,7 +1,9 @@
 package com.example.springoauth;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class TestApi {
     @GetMapping("/test")
     public String index() {
