121e501e63ca19d06db1bab404375c11bc99a4bf
ggongdori <soominlee0410@gmail.com>
2022-05-18 23:57:03 +0900

    [feat] geolocation impl

---
+++ geolocationApi/src/main/java/com/example/geolocationapi/Location.java
@@ -0,0 +1,12 @@
+package com.example.geolocationapi;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+
+    @Id
+    private double latitude;
+    private double longitude;
+}
