dd4b80dc30f6fe06e7624a85dc040390ab1d262b
ggongdori <soominlee0410@gmail.com>
2022-05-19 23:20:18 +0900

    [feat] handler, config classes

--- websocket/src/main/java/com/example/websocket/Greeting.java
+++ websocket/src/main/java/com/example/websocket/Greeting.java
@@ -1,8 +1,10 @@
 package com.example.websocket;
 
+import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 public class Greeting {
     private String content;
 
